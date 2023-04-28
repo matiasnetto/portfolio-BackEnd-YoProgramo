1048d9af5724e42385c375bdba4ada9d5dfeb687
Matias Netto <matiasnetto03@gmail.com>
2023-04-28 13:38:01 -0300

    Refactor projects model, add technologies relation

--- src/main/java/ar/com/matiasnetto/portfolio/models/Projects.java
+++ src/main/java/ar/com/matiasnetto/portfolio/models/Projects.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.sql.Date;
+import java.util.Set;
 
 @Data
 @Entity @Table(name = "projects")
@@ -18,4 +19,10 @@ public class Projects {
     private String project_url;
     private String github_url;
     private int ord;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "technology_in_project",
+            joinColumns = @JoinColumn(name = "project_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "technology_id", referencedColumnName = "id"))
+    private Set<Skills> technologies;
 }

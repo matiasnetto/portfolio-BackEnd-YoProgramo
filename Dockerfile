8b1fef901699b2ed0b294a264332c0cdbdde63fc
Matias Netto <matiasnetto03@gmail.com>
2023-05-14 18:27:00 -0300

    Add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM amazoncorretto:17
+MAINTAINER matiasnetto
+COPY target/portfolio-0.0.1-SNAPSHOT.jar portfolio-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java","-jar","/portfolio-0.0.1-SNAPSHOT.jar"]
---
+++ target/classes/database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE portfolio;
+
+INSERT INTO skills (technology, image_url, percent, ord) VALUES ("Javascript", "https://javascribp.com/img.jpg", 80, 1), ("Angular", "https://angular.com/img.jpg", 50, 2), ("Java", "https://java.com/img.jpg", 50, 2);
+
+ALTER TABLE skills modify COLUMN id INT AUTOINCREMENT;
+
+DELETE FROM technology_in_project WHERE technology_id == 1;
+
